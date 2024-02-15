a4aa9c2ef89ea5f84ef1abd305831058e30d21be
Kennedy Musau <kennedy.musauu@gmail.com>
2024-02-15 22:41:47 +0300

    add house endpoints

--- app/app.py
+++ app/app.py
@@ -67,5 +67,53 @@ def estate_by_id(id):
         }, 200)
 
 
+@app.route("/houses", methods=["GET", "POST"])
+def houses():
+    if request.method == "GET":
+
+        return make_response(jsonify([estate.to_dict() for estate in Estate.query.all()]), 200)
+
+    elif request.method == "POST":
+        new_house = House(
+            tenant=request.form.get("tenant"),
+            num_of_rooms=request.form.get("num_of_rooms"),
+            price=request.form.get("price"),
+            estate_id=request.form.get("estate_id")
+        )
+
+        db.session.add(new_house)
+        db.session.commit()
+
+        house_dict = new_house.to_dict()
+        return make_response(jsonify(house_dict), 200)
+
+
+@app.route("/houses/<int:id>", methods=["GET", "PATCH", "DELETE"])
+def houe_by_id(id):
+
+    house = House.query.filter_by(id == id).first()
+
+    if request.method == "GET":
+        house_dict = house.to_dict()
+        return make_response(jsonify(house_dict), 200)
+
+    elif request.method == "PATCH":
+        for attr in request.form:
+            setattr(house, attr, request.form.get(attr))
+
+            db.session.add(house)
+            db.session.commit()
+
+            house_dict = house.to_dict()
+
+            return make_response(jsonify(house_dict), 200)
+
+    elif request.method == "DELETE":
+        db.session.delete(house)
+        db.session.commit()
+
+        return make_response(["House was successfully deleted"])
+
+
 if __name__ == "__main__":
     app.run(port=5555, debug=True)
